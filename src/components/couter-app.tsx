159e98c02b973e321360c98595de59fd9d9d0f78
dev-bilaspure <devbilaspure4941@gmail.com>
2024-09-21 23:01:29 +0530

    Update example

--- src/components/couter-app.tsx
+++ src/components/couter-app.tsx
@@ -4,14 +4,15 @@ import { Button } from "@/components/ui/button";
 import toast from "react-hot-toast";
 
 export default function CounterApp() {
-  const [count, setCount] = useUndoRedoState(0, {
-    onUndo: () => {
-      toast.success("Undo successful");
-    },
-    onRedo: () => {
-      toast.success("Redo successful");
-    },
-  });
+  const [count, setCount, undo, redo, resetCount, clearHistory] =
+    useUndoRedoState(0, {
+      onUndo: () => {
+        toast.success("Undo successful");
+      },
+      onRedo: () => {
+        toast.success("Redo successful");
+      },
+    });
   return (
     <div className="w-full h-screen flex flex-col gap-10 items-center justify-center">
       <div className="w-full flex flex-col gap-4 items-center justify-center">
@@ -20,18 +21,35 @@ export default function CounterApp() {
       </div>
       <div className="flex flex-col gap-5 items-center">
         <p className="text-2xl">{count}</p>
-        <div className="flex items-center gap-5">
-          <Button
-            variant={"outline"}
-            onClick={() => {
-              setCount(count - 1);
-            }}
-          >
-            - Decrement
-          </Button>
-          <Button variant={"outline"} onClick={() => setCount(count + 1)}>
-            + Increment
-          </Button>
+        <div className="flex flex-col gap-5 ">
+          <div className="flex items-center gap-5 justify-center">
+            <Button
+              variant={"outline"}
+              onClick={() => {
+                setCount(count - 1);
+              }}
+            >
+              - Decrement
+            </Button>
+            <Button variant={"outline"} onClick={() => setCount(count + 1)}>
+              + Increment
+            </Button>
+          </div>
+
+          <div className="flex items-center gap-5  justify-center">
+            <Button variant={"outline"} onClick={() => undo()}>
+              Undo
+            </Button>
+            <Button variant={"outline"} onClick={() => redo()}>
+              Redo
+            </Button>
+            <Button variant={"outline"} onClick={() => resetCount()}>
+              Reset
+            </Button>
+            <Button variant={"outline"} onClick={() => clearHistory()}>
+              Clear History
+            </Button>
+          </div>
         </div>
       </div>
     </div>
